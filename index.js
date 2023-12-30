b624811fee4982b58a201462bdba0477a97074c0
Josh <hidefromban@gmail.com>
2023-12-30 01:14:01 -0300

    metodo post

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ const express = require('express')
 const app = express();
 const port = 3000;
 
+
+app.use(express.json());
+
+
+routerApi(app);
 app.get('/', (req,res)=> {
   res.send('Hola mi server en express')
 })
 app.get('/nueva-ruta', (req,res)=> {
   res.send('Hola soy el nuevo endpoint')
 })
-routerApi(app);
 
 
 app.listen(port,()=>{
